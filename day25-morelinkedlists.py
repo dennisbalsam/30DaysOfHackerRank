610d073b605c83b2c7333a910f77ec310e7212f8
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-04-05 16:50:29 -0400

    day 25

---
+++ day25-morelinkedlists.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def insert(self,head,data):
+            p = Node(data)           
+            if head==None:
+                head=p
+            elif head.next==None:
+                head.next=p
+            else:
+                start=head
+                while(start.next!=None):
+                    start=start.next
+                start.next=p
+            return head  
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def removeDuplicates(self,head):
+        #Write your code here
+        currentnode = head
+        while currentnode != None and currentnode.next != None:
+            while currentnode.next != None and currentnode.data == currentnode.next.data:
+                currentnode.next = currentnode.next.next
+            currentnode = currentnode.next
+        return head
+mylist= Solution()
